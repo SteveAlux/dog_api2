260240600dd841a8f7664657c9de24a36042cbb5
Steve Velasco <Steve16velasco@gmail.com>
2019-05-03 14:17:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,23 @@ function handleSubmit(){
     $('#dog_form').submit(function(){
         event.preventDefault();
         let dog_num = $('#dog_number').val();
-        let select = $('#select_breed').val();
+        
         console.log($('#dog_number').val());
-        fetch(`https://dog.ceo/api/breed/${select}/images/random`)
+        fetch(`https://dog.ceo/api/breed/${dog_num}/images/random`)
         .then(response => response.json())
         .then(responseJson => displayImages(responseJson))
-        .catch(error => alert('Something went wrong. Try again later.'));
+        .catch(error => alert(`Something went wrong. Try again later.${error}`));
         
     });
 }
 
 function displayImages(responseJson){
+
     let message = responseJson.message;
+    if (message === 'Breed not found'){
+        alert(`Breed not found, try: 'boxer' ` );
+    }
+    else{
     let emptyArray = [];
     console.log(message)
     console.log(message[1]);
@@ -22,6 +27,7 @@ function displayImages(responseJson){
      
         emptyArray.push(`<img src='${message}'>`);
         $('#landing').html(emptyArray);
+    }
 }
 
 
